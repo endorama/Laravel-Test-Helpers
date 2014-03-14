f2bf74dfeb5759b1120811ba04f9de4cd30694b8
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-03-14 11:06:39 +0100

    add comments

--- src/Way/Tests/ModelHelpers.php
+++ src/Way/Tests/ModelHelpers.php
@@ -4,63 +4,105 @@ use Mockery;
 
 trait ModelHelpers {
 
-    public function tearDown()
-    {
+    /**
+     * Add teardown method to class
+     */
+    public function tearDown() {
         Mockery::close();
     }
 
-    public function assertValid($model)
-    {
-        $this->assertRespondsTo('validate', $model, "The 'validate' method does not exist on this model.");
-        $this->assertTrue($model->validate(), 'Model did not pass validation.');
+    # method helpers
+    /**
+     * Check if $class->$method() exists in $class.
+     * 
+     * Check for private / protected methods, and this is the intended behaviour.
+     * Infact a $class responds to $method even if is private and you cannot use
+     * it outside.
+     * See http://us1.php.net/manual/en/function.method-exists.php for other
+     * options
+     */
+    public function assertRespondsTo($method, $class, $message = null) {
+      $message = $message ?: "Expected the '$class' class to have method, '$method'.";
+
+      $this->assertTrue(method_exists($class, $method), $message);
     }
 
-    public function assertNotValid($model)
-    {
-        $this->assertRespondsTo('validate', $model, "The 'validate' method does not exist on this model.");
-        $this->assertFalse($model->validate(), 'Did not expect model to pass validation.');
+    # validation helpers
+
+    /**
+     * Assert $model validity.
+     * 
+     * This expect the model to respond to a validate() method.
+     * See https://github.com/JeffreyWay/Laravel-Model-Validation
+     */
+    public function assertValid($model) {
+      $this->assertRespondsTo('validate', $model, "The 'validate' method does not exist on this model.");
+      $this->assertTrue($model->validate(), 'Model did not pass validation.');
     }
 
-    public function assertBelongsToMany($parent, $child)
-    {
-        $this->assertRelationship($parent, $child, 'belongsToMany');
+    /**
+     * Assert $model invalidity.
+     * 
+     * This expect the model to respond to a validate() method.
+     * See https://github.com/JeffreyWay/Laravel-Model-Validation
+     */
+    public function assertNotValid($model) {
+      $this->assertRespondsTo('validate', $model, "The 'validate' method does not exist on this model.");
+      $this->assertFalse($model->validate(), 'Did not expect model to pass validation.');
     }
 
-    public function assertBelongsTo($parent, $child)
-    {
-        $this->assertRelationship($parent, $child, 'belongsTo');
+    # relationship helpers
+
+    public function assertBelongsToMany($parent, $child) {
+      $this->assertRelationship($parent, $child, 'belongsToMany');
     }
 
-    public function assertHasMany($relation, $class)
-    {
-        $this->assertRelationship($relation, $class, 'hasMany');
+    public function assertBelongsTo($parent, $child) {
+      $this->assertRelationship($parent, $child, 'belongsTo');
     }
 
-    public function assertHasOne($relation, $class)
-    {
-        $this->assertRelationship($relation, $class, 'hasOne');
+    public function assertHasMany($relation, $class) {
+      $this->assertRelationship($relation, $class, 'hasMany');
     }
 
-    public function assertMorphMany($relation, $class, $morphable)
-    {
-        $this->assertRelationship($relation, $class, 'morphMany');
+    public function assertHasOne($relation, $class) {
+      $this->assertRelationship($relation, $class, 'hasOne');
     }
 
-    public function assertMorphTo($relation, $class)
-    {
-        $this->assertRelationship($relation, $class, 'morphTo');
+    public function assertMorphMany($relation, $class, $morphable) {
+      $this->assertRelationship($relation, $class, 'morphMany');
     }
 
-    public function assertRespondsTo($method, $class, $message = null)
-    {
-        $message = $message ?: "Expected the '$class' class to have method, '$method'.";
-
-        $this->assertTrue(
-            method_exists($class, $method),
-            $message
-        );
+    public function assertMorphTo($relation, $class) {
+      $this->assertRelationship($relation, $class, 'morphTo');
     }
 
+    /**
+     * Assert simple relationship in $class
+     * 
+     * @param $relationship the relationship method in the model
+     * @param $class the model class
+     * @param $type the relationship type ( BelongsTo, BelongsToMany, haveMany ... )
+     */
+    public function assertRelationship($relationship, $class, $type) {
+      // check that class respond to relationship method
+      $this->assertRespondsTo($relationship, $class);
+
+      // create mocked object ( baseClass is the relationship )
+      $mocked = $this->createRelationshipMock($relationship, $class, $type, $relationship);
+
+      // run method to perform assertion
+      $class->$relationship();
+    }
+
+    /**
+     * In case of a recursive relation ( so model has one / many self )
+     * you can use this assertion.
+     * 
+     * @param $relationship the relationship method in the model
+     * @param $class the model class
+     * @param $type the relationship type ( BelongsTo, BelongsToMany, haveMany ... )
+     */
     public function assertRecursiveRelationship($relationship, $class, $type) {
       // check that class respond to relationship method
       $this->assertRespondsTo($relationship, $class);
@@ -76,18 +118,9 @@ trait ModelHelpers {
       $mocked->$relationship();
     }
 
-    public function assertRelationship($relationship, $class, $type) {
-      // check that class respond to relationship method
-      $this->assertRespondsTo($relationship, $class);
+    # privates
 
-      // create mocked object ( baseClass is the relationship )
-      $mocked = $this->createRelationshipMock($relationship, $class, $type, $relationship);
-
-      // run method to perform assertion
-      $class->$relationship();
-    }
-
-    public function getArgumentsRelationship($relationship, $class, $type) {
+    private function getArgumentsRelationship($relationship, $class, $type) {
         $mocked = Mockery::mock($class."[$type]")->shouldIgnoreMissing()->asUndefined();
 
         $args = array();
