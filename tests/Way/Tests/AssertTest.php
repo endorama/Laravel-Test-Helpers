cf0b50840eae62cb78b858710c6ee6246ab5b167
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-03-14 14:34:41 +0100

    add test for Assert and Should

---
+++ tests/Way/Tests/AssertTest.php
@@ -0,0 +1,68 @@
+<?php
+
+class AssertTest extends PHPUnit_Framework_TestCase {
+
+  protected $class = 'Way\Tests\Assert';
+
+  // public function setUp() {}
+  // public function tearDown() {}
+
+  public function testShouldRespondToHave() {
+    $this->doMethodCheck('have');
+  }
+
+  public function testShouldRespondToEq() {
+    $this->doMethodCheck('eq');
+  }
+
+  public function testShouldRespondToBeValid() {
+    $this->doMethodCheck('beValid');
+  }
+
+  public function testShouldRespondToBeNotValid() {
+    $this->doMethodCheck('beNotValid');
+  }
+
+  public function testShouldRespondToBelongsToMany() {
+    $this->doMethodCheck('belongsToMany');
+  }
+
+  public function testShouldRespondToBelongsTo() {
+    $this->doMethodCheck('belongsTo');
+  }
+
+  public function testShouldRespondToHaveMany() {
+    $this->doMethodCheck('haveMany');
+  }
+
+  public function testShouldRespondToHaveOne() {
+    $this->doMethodCheck('haveOne');
+  }
+
+  public function testShouldRespondToMorphMany() {
+    $this->doMethodCheck('morphMany');
+  }
+
+  public function testShouldRespondToMorphTo() {
+    $this->doMethodCheck('morphTo');
+  }
+
+  public function testShouldRespondToRespondTo() {
+    $this->doMethodCheck('respondTo');
+  }
+
+  public function testShouldRespondToHaveRecursiveRelationship() {
+    $this->doMethodCheck('haveRecursiveRelationship');
+  }
+
+  public function testShouldRespondToHaveRelationship() {
+    $this->doMethodCheck('haveRelationship');
+  }
+
+  # privates 
+
+  private function doMethodCheck($method) {
+    $msg = "Expected the '$this->class' class to have method, '$method'.";
+    $this->assertTrue(is_callable($this->class . '::' . $method), $msg);
+  }
+}
